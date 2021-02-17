d354144e26816f891ac442e2d2405b466d92bf79
Zuhair Ali <zuhairali20003@gmail.com>
2021-02-17 10:49:07 -0600

    Initial Commit

---
+++ Programming Jokes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Programming Jokes
+//
+//  Created by Student on 2/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Programming Jokes/Programming_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_JokesApp.swift
+//  Programming Jokes
+//
+//  Created by Student on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Programming_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
